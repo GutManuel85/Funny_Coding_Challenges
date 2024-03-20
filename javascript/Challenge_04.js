1f1efb522ceb4ed97581213a172ea95d333fa7d7
GutManuel85 <72383983+GutManuel85@users.noreply.github.com>
2024-03-20 17:48:54 +0100

    Better performance

--- javascript/Challenge_04.js
+++ javascript/Challenge_04.js
@@ -4,7 +4,7 @@ const checkPalindrom = function(text, isIgnoredCase){
     if(isIgnoredCase){
         text = text.toLowerCase();
     }
-    for(let i = 0; i <= text.length; i++){
+    for(let i = 0; i <= text.length / 2; i++){
         if(!(text[i] === text[text.length - 1 -i])){
             return false;
         }
